87278efb7b063fd442c59674087ce0c1c1e629b1
Onuchi Johnson <Onuchi.J@bullseye.local>
2023-05-26 13:41:44 +0100

    new lines

--- Documents/BULLSEYE/DJANGO/myblog/blueblog/blogs/views.py
+++ Documents/BULLSEYE/DJANGO/myblog/blueblog/blogs/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .forms import CommentForm
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
-def detail(request, slug):
+def detail(request, category_slug, slug):
     post = get_object_or_404(Post, slug=slug)
 
-    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+
     return render(request, 'blogs/detail.html', {'post':post, 'form':form})
+
+
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+
+    return render(request, 'blogs/category.html', {'category': category})
